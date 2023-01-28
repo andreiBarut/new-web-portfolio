239ddb1e5b04fd4a5c2484b19dad405800f7ae99
Andrei B <andreibarut@gmail.com>
2023-01-28 21:38:10 +0200

    added projects page and linked in popup

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import "./Home.css";
+import { AiFillLinkedin } from "react-icons/ai";
 
 export default function Home() {
 	const [pipe, setPipe] = useState("|");
+	const stars = [
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+		"ten",
+	];
 	setTimeout(() => {
 		if (pipe.includes("|")) {
 			setPipe(" ");
@@ -11,19 +24,26 @@ export default function Home() {
 		setPipe("|");
 	}, 500);
 	return (
-		<section className="home-container">
-			<h1>hi, my name is</h1>
-			<p>
-				<span className="home-myname-span">Andrei Barut</span>
-				<br></br>I am software developer, passionate, open to a new professional
-				challenge and ready to take on responsibilities. Reliable team member always
-				ready to help. Quick learner with problem-solving and decision-making
-				mindset, and excellent interpersonal skills, I am seeking to be employed in
-				a company with strong organizational culture, where I can add value using my
-				skills and my previous professional experience within the IT Industry.
-				<span className="home-pipe-span"> |</span>
-			</p>
-			<div className="home-resume-container">resume</div>
-		</section>
+		<>
+			<section className="home-container">
+				<h1>hi, my name is</h1>
+				<p>
+					<span className="home-myname-span">Andrei Barut</span>
+					<br></br>I am software developer, passionate, open to a new professional
+					challenge and ready to take on responsibilities. Reliable team member
+					always ready to help. Quick learner with problem-solving and
+					decision-making mindset, and excellent interpersonal skills, I am seeking
+					to be employed in a company with strong organizational culture, where I can
+					add value using my skills and my previous professional experience within
+					the IT Industry.
+					<span className="home-pipe-span"> |</span>
+				</p>
+				<div className="home-resume-container">resume</div>
+			</section>
+			<div className="line"></div>
+			<div className="circle">
+				<AiFillLinkedin />
+			</div>
+		</>
 	);
 }
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,14 +1,28 @@
+import "./Projects.css";
+
 export default function Projects() {
 	return (
 		<div className="projects-container">
-			<h1>My Projects</h1>
+			<h2>My Projects</h2>
 			<ul>
-				<li>Project 1</li>
-				<li>Project 1</li>
-				<li>Project 1</li>
-				<li>Project 1</li>
-				<li>Project 1</li>
-				<li>Project 1</li>
+				<li>
+					<a href="#">Grandma's Recipes</a>
+				</li>
+				<li>
+					<a href="#">Currency Exchange</a>
+				</li>
+				<li>
+					<a href="#">Hangman with Cars</a>
+				</li>
+				<li>
+					<a href="#">Custom Digital Watch</a>
+				</li>
+				<li>
+					<a href="#">Custom Voting Polls</a>
+				</li>
+				<li>
+					<a href="#">This React Portfolio</a>
+				</li>
 			</ul>
 		</div>
 	);
