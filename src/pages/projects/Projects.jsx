09c00f2eb5a8dee12d58c1fc7dd29af6ccae4196
Andrei B <andreibarut@gmail.com>
2023-02-01 18:33:23 +0200

    refactored projects section improved

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,5 +1,5 @@
 import "./Projects.css";
-import { AiFillCaretDown } from "react-icons/ai";
+import { AiFillCaretDown, AiOutlineLink } from "react-icons/ai";
 // import {
 // 	Accordion,
 // 	AccordionItem,
@@ -7,32 +7,42 @@ import { AiFillCaretDown } from "react-icons/ai";
 // 	AccordionItemButton,
 // 	AccordionItemPanel,
 // } from "react-accessible-accordion";
-
+import { AiFillLinkedin } from "react-icons/ai";
+import db from "../../db.json";
 // import "react-accessible-accordion/dist/fancy-example.css";
 import grandma from "../../assets/images/grandmasRecipes.png";
 export default function Projects() {
 	return (
 		<div className="projects-container" id="projects">
 			<h2>My Projects</h2>
-			<article className="projects-featured-article">
-				<div className="projects-featured-article-img"></div>
-				<div className="projects-featured-description-container">
-					<p className="projects-featured-project-paragraph">Featured Project</p>
-					<h1>Grandma's Recipes</h1>
-					<p className="projects-featured-project-description-paragraph">
-						Cooking assistant made with react, json as database, react router dom for
-						dynamic routing. This project is an homage to my grandmother, who was a
-						cook during the communist period. The recipes are taken from her cookbook.
-					</p>
-					<ul>
-						<li>JS</li>
-						<li>React</li>
-						<li>HTML</li>
-						<li>CSS</li>
-						<li>json</li>
-					</ul>
-				</div>
-			</article>
+			{/* {db.projects[0].title} */}
+			{db.projects.map((project) => (
+				<article className="projects-featured-article">
+					<div className="projects-featured-article-img"></div>
+					<div className="projects-featured-description-container">
+						<p className="projects-featured-project-paragraph">
+							Featured Project #{project.id}
+						</p>
+						<h1>{project.title}</h1>
+						<p className="projects-featured-project-description-paragraph">
+							{project.description}
+						</p>
+						<ul>
+							{project.technologies.map((technology) => (
+								<li>{technology}</li>
+							))}
+						</ul>
+						<ul>
+							<li>
+								<AiFillLinkedin className="projects-link" />
+							</li>
+							<li>
+								<AiOutlineLink className="projects-link" />
+							</li>
+						</ul>
+					</div>
+				</article>
+			))}
 		</div>
 	);
 }
