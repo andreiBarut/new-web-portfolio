fa770065ca9c0a4ce1851181c23c0d31f9f78a56
Andrei B <andreibarut@gmail.com>
2023-05-24 11:20:51 +0300

    updated content 24th May

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -17,16 +17,11 @@ export default function About() {
 					<Fade direction="bottom" duration={2000}>
 						<p>
 							Over two years experience working with HTML, CSS (flexbox, grid layouts,
-							bootstrap, SCSS), one year of working with JavaScript, over 6 months
-							working with React. I focused on developing my programming skills with
+							bootstrap, SCSS, Tailwind CSS), one year working with front-end
+							technologies. I focused on developing my programming skills with
 							JavaScript and React the past year, when I attended an 8 month intensive{" "}
-							<span className="highlighted">JavaScript coding bootcamp at SDA</span>.
-						</p>
-					</Fade>
-					<Fade>
-						<p>
-							This allowed me to learn many things over the course of 8 months,
-							including
+							<span className="highlighted">JavaScript coding bootcamp at SDA</span>{" "}
+							which allowed me to delve deeper into front-end technologies, such as{" "}
 							<span className="highlighted"> JavaScript and React</span>, among other
 							technologies, such as
 							<span className="highlighted"> HTTP methods</span>, request methods in
@@ -35,8 +30,34 @@ export default function About() {
 							and communicate with a server or
 							<span className="highlighted"> Web API. </span>Attending the course also
 							helped me develop team work and debugging skills, by doing live coding
-							sessions with my colleagues and trainers as well. Here are the skills I
-							learned over the past year:
+							sessions with my colleagues and trainers as well.
+						</p>
+					</Fade>
+					<Fade>
+						<p>
+							Fast forwarding to post coding bootcamp, I am now focusing on learning{" "}
+							<span className="highlighted">new technologies & skills</span>, by
+							applying them to building actual applications. I have developed a CRUD
+							custom voting application for AMVVD which is used by the local county
+							organization for making decisions regarding important situations.
+						</p>
+						<p>
+							Currently developing <span className="highlighted">EMERDEN</span>, a PC
+							gaming deals marketplace platform that allows users to discover the best
+							bargains from approximately 30 diverse stores. Within this platform,
+							users can effortlessly navigate the marketplace, curate their personal
+							wishlist of games, monitor price fluctuations over time, and access data
+							visualization tools for an in-depth view of price evolution.
+							Additionally, they can receive personalized email notifications for their
+							tracked games. This full-stack application is crafted using the{" "}
+							<span className="highlighted">MERN </span>
+							stack, which includes <span className="highlighted">Mongo DB</span> for
+							the database, RESTful <span className="highlighted">API</span>,{" "}
+							<span className="highlighted">Express and Node.js</span> for the backend,
+							<span className="highlighted">React</span> for the frontend,{" "}
+							<span className="highlighted">MUI</span> for component-based layouts.
+							<span className="highlighted"> React Router DOM</span> was employed for
+							seamless dynamic routing.
 						</p>
 					</Fade>
 					<Fade>
@@ -50,6 +71,20 @@ export default function About() {
 
 								<li>React.js</li>
 
+								<li>Node.js</li>
+
+								<li>Express</li>
+
+								<li>Mongo DB</li>
+
+								<li>Mongoose</li>
+
+								<li>Firebase</li>
+
+								<li>MUI</li>
+
+								<li>Linux</li>
+
 								<li>Bootstrap</li>
 
 								<li>Tailwind CSS</li>
@@ -61,6 +96,8 @@ export default function About() {
 								<li>Figma</li>
 
 								<li>SQL</li>
+
+								<li>Version Control</li>
 							</ul>
 						</article>
 					</Fade>
